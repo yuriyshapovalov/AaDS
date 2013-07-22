37e214365eb1b29737f22579ca63ad9632c4f041
Yuriy Shapovalov <shapovalov.yuri@gmail.com>
2013-07-22 23:31:19 +0300

    added VS project for _tests

---
+++ _tests/sorting/bubble-sort-tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace sorting
+{
+    [TestClass]
+    public class bubble_sort_tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
