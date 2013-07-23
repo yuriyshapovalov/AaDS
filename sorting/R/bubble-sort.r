c1854cca1cb5a3d2b9c5502275818bafb77a3e65
Yuriy Shapovalov <shapovalov.yuri@gmail.com>
2013-07-23 23:27:14 +0300

    Minor changes commit

---
+++ sorting/R/bubble-sort.r
@@ -0,0 +1,16 @@
+# bubble sort implementation
+
+RandSequence <- sample(1:1000, 1000, replace=T);
+
+bubbleSort <- function(sequenece) {
+    clear <- TRUE;
+    while(clear) {
+        clear <- FALSE;
+    
+        for(i in sequence) {
+        
+            
+        }
+    }
+}
+
