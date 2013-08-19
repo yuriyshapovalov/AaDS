9bea456b47529297185daf1648049364d525eb60
Yuriy Shapovalov <shapovalov.yuri@gmail.com>
2013-08-20 01:00:19 +0300

    Updated code generation stubs for C#

--- _builder/autogen/csCode.bat
+++ _builder/autogen/csCode.bat
@@ -1,8 +1,20 @@
 @echo off
 
-echo.namespace AaDS.%1  >> text.txt
-echo.{  >> text.txt
-echo.	public class %2  >> text.txt
-echo.	{} >> text.txt
-echo.}  >> text.txt
-
+echo.using System;					>> %1
+echo. 								>> %1
+echo.namespace AaDS.%2				>> %1
+echo.{								>> %1
+echo.	/// <summary>				>> %1
+echo.	/// %3						>> %1
+echo.	/// </summary>				>> %1
+echo.	/// O(n) ~ ...				>> %1
+echo.	///							>> %1
+echo.	public class %3				>> %1
+echo.	{							>> %1
+echo.		public %3()				>> %1
+echo.		{}						>> %1
+echo.								>> %1
+echo.		/// <summary></summary>	>> %1
+echo.		/// <method>			>> %1
+echo.	}							>> %1
+echo.}								>> %1
--- _builder/autogen/csTest.bat
+++ _builder/autogen/csTest.bat
@@ -1,8 +1,26 @@
 @echo off
 
-echo.namespace AaDS.%1  >> text.txt
-echo.{  >> text.txt
-echo.	public class %2  >> text.txt
-echo.	{} >> text.txt
-echo.}  >> text.txt
-
+echo.using NUnit.Framework;													> %1
+echo.using AaDS.%3;															> %1
+echo.using AaDS.DataProvider;												> %1
+echo.																		> %1
+echo.[TestFixture]															> %1
+echo.public class %2Test													> %1
+echo.{																		> %1
+echo.	// private %2 _testInstance; 										> %1
+echo.	private DataProvider _provider;										> %1
+echo.																		> %1
+echo.	[SetUp]																> %1
+echo.	public void Init()													> %1
+echo.	{																	> %1
+echo.		// this._testInstance = new %2();								> %1
+echo.		this._provider = DataProvider.GetDataProvider();				> %1
+echo.	}																	> %1
+echo.																		> %1
+echo.	// remove it after real code will be created						> %1
+echo.	[Test]																> %1
+echo.	public void AlwaysFailedTestStub()									> %1
+echo.	{																	> %1
+echo.		Assert.Fail();													> %1
+echo.	}																	> %1
+echo.}																		> %1
