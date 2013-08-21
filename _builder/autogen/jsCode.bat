0904e41eb93f1f31619a42037d12aea5c22bdb41
Yuriy Shapovalov <shapovalov.yuri@gmail.com>
2013-08-21 10:00:02 +0300

    Updated code gen files

--- _builder/autogen/jsCode.bat
+++ _builder/autogen/jsCode.bat
@@ -2,7 +2,7 @@
 
 "use strict";
 
-echo.// %2>>%1
+echo.// %3 - %2>>%1
 echo.var %2 = function() {>>%1
 echo.	// vars>>%1
 echo.>>%1
--- _builder/autogen/once_run_creation.bat
+++ _builder/autogen/once_run_creation.bat
@@ -2,8 +2,66 @@
 ; preventing accidancly run
 goto exit
 
-; _call_ _FolderName_ _Namespace_ _ClassName_
-call create sorting Sorting BubbleSort
-call create sorting Sorting TimSort
+; _call_ _FolderName_ _ClassName_ _Namespace_
+call create sorting BubbleSort Sorting
+call create sorting TimSort Sorting
+
+;Sorting
+
+call create sorting AdaptiveSort Sorting
+call create sorting AmericanFlagSort Sorting
+call create sorting BeadSort Sorting
+call create sorting BubbleSort Sorting
+call create sorting BucketSort Sorting
+call create sorting BurstSort Sorting
+call create sorting CoctailSort Sorting
+call create sorting CombSort Sorting
+call create sorting CountingSort Sorting
+call create sorting CycleSort Sorting
+call create sorting FlashSort Sorting
+call create sorting GnomeSort Sorting
+call create sorting HeapSort Sorting
+call create sorting InsertionSort Sorting
+call create sorting IntroSort Sorting
+call create sorting JSort Sorting
+call create sorting LibrarySort Sorting
+call create sorting MergeSort Sorting
+call create sorting OddEvenSort Sorting
+call create sorting PancakeSort Sorting
+call create sorting PatienceSort Sorting
+call create sorting PigeonholeSort Sorting
+call create sorting ProxmaxSort Sorting
+call create sorting QuickSort Sorting
+call create sorting RadixSort Sorting
+call create sorting SimpleSort Sorting
+call create sorting SelectionSort Sorting
+call create sorting ShellSort Sorting
+call create sorting SmoothSort Sorting
+call create sorting SpreadSort Sorting
+call create sorting StandSort Sorting
+call create sorting StoogeSort Sorting
+call create sorting TimSort Sorting
+call create sorting TournamentSort Sorting
+call create sorting TreeSort Sorting
+call create sorting UnshuffleSort Sorting
+
+; Trees
+call create trees AATree Trees
+call create trees AVLTree Trees
+call create trees BinaryHeap Trees
+call create trees CartesianTree Trees
+call create trees HashTree Trees
+call create trees LLRBTree Trees
+call create trees RadixTree Trees
+call create trees RedBlackTree Trees
+call create trees ScapegoatTree Trees
+call create trees SplayTree Trees
+call create trees SuffixTree Trees
+call create trees TernarySearchTree Trees
+call create trees TopTree Trees
+call create trees Treap Trees
+call create trees TwoThreeTree Trees
+call create trees XFastTree Trees
+call create trees YFastTree Trees
 
 :exit
