23a45d9afd194087f124f1346b9fd69d89fd3bc4
Yuriy Shapovalov <shapovalov.yuri@gmail.com>
2013-08-23 17:34:46 +0300

    Updated unit tests for LinkedList on C#

--- _tests/lists/csharp/LinkedListTest.cs
+++ _tests/lists/csharp/LinkedListTest.cs
@@ -8,22 +8,82 @@ public class LinkedListTest
 
     [SetUp]
     public void Init() {
-        list = new LinkedList<int>();
+       // list = new LinkedList<int>();
     }
 
 	[Test]
-	public void LinkedListGenericContainerTest()
+	public void LinkedList_Add_IntContainerTest()
 	{
-        LinkedList<int> list = new LinkedList<int>();
+        	LinkedList<int> list = new LinkedList<int>();
 		list.add(1);
 		list.add(2);
 		list.add(3);
 
 		Assert.AreEqual(list.Length, 3);
-
 		Assert.AreEqual(list.value, 1);
-
 		Assert.AreEqual(list.Next().value, 2);
+		Assert.AreEqual(list.Next().Next().value, 3);
+		Assert.AreEqual(list.Next().Next().Next(), null);
+	}
+
+	[Test]
+	public void LinkedList_Remove_IntContainerTest()
+	{
+		LinkedList<int> list = new LinkedList<int>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+
+		Assert.AreEqual(list.Length, 4);
 		
+		list.remove(4);
+		// 1 -> 2 -> 3 -> null 
+		Assert.AreEqual(list.Length, 3);
+		Assert.AreEqual(list.value, 1);
+		Assert.AreEqual(list.Next().value, 2);
+		Assert.AreEqual(list.Next().Next().value, 3);
+		Assert.AreEqual(list.Next().Next().Next(), null);
+
+		list.remove(2);
+		// 1 -> 3 -> null
+		Assert.AreEqual(list.Length, 2);
+		Assert.AreEqual(list.value, 1);
+		Assert.AreEqual(list.Next().value, 3);
+		Assert.AreEqual(list.Next().Next(), null);		
+
+		list.remove(1);
+		//  3 -> null
+		Assert.AreEqual(list.Length, 1);
+		Assert.AreEqual(list.value, 3);
+		Assert.AreEqual(list.Next(), null);
+
+		list.remove(3);
+		// null
+		Assert.AreEqual(list.Length, 0);
+		Assert.AreEqual(list.value, null);
+	}
+
+	[Test]
+	public void LinkedList_Length_IntContainerTest()
+	{
+		LinkedList<int> list = new LinkedList<int>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+
+		// 1 -> 2 -> 3 -> null
+		Assert.AreEqual(list.Length, 3);
+
+		list.add(4);
+		
+		// 1 -> 2 -> 3 -> 4 -> null
+		Assert.AreEqual(list.Length, 4);
+
+		list.remove(4);
+		list.remove(3);
+
+		// 1-> 2 -> null
+		Assert.AreEqual(list.Length, 2);
 	}
 }
