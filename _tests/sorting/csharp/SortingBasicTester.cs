b584c5edefa7108be93cff25be546f45595f318f
Yuriy Shapovalov <shapovalov.yuri@gmail.com>
2013-08-30 09:51:17 +0300

    Added test stubs for common tests

---
+++ _tests/sorting/csharp/SortingBasicTester.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+using AaDS.Sorting;
+using AaDS.DataProvider;
+
+[TestFixture]
+public class SortingBasicTester
+{
+    protected ISort _sorter;
+
+    [SetUp]
+    public virtual void Init()
+    {
+        Assert.Ignore();
+    }
+
+
+}
---
+++ _tests/sorting/csharp/SortingPerformanceTester.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+using AaDS.Sorting;
+using AaDS.DataProvider;
+
+[TestFixture]
+public class SortingPerformanceTester
+{
+    protected ISort _sort;
+
+    [SetUp]
+    public virtual void Init()
+    {
+        Assert.Ignore();
+    }
+}
