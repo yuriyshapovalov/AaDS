1b7de09d266cfea32c4c785b80629ec42ac78a41
Yuriy Shapovalov <shapovalov.yuri@gmail.com>
2013-09-23 00:01:20 +0300

    Update unittests for Python project

--- _tests/sorting/python/BubbleSortTest.py
+++ _tests/sorting/python/BubbleSortTest.py
@@ -1,11 +1,20 @@
+__author__ = 'Yuriy Shapovalov (shapovalov.yuri@gmail.com)'
+
 import unittest
+#from sorting import BubbleSort
 
 class BubbleSortTest(unittest.TestCase):
 
-	# def setUp(self):
+    _data = []
 
-	def test_BubbleSortStub(self):
-		self.failIf(True)
+    def setUp(self):
+        with open('../../../_data/rand10_positive_norepear.data', 'r') as f:
+            seq = f.read()
+            _data = seq.split(sep=' ')
+        print(_data)
+
+    def test_BubbleSortStub(self):
+        self.assertTrue(True)
 
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
