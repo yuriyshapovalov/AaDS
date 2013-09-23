f01ca18974bbd2667a14e2f19cd8e8a4156ecc37
Yuriy Shapovalov <shapovalov.yuri@gmail.com>
2013-09-24 00:00:33 +0300

    Updated python build script

--- _builder/buildpython.bat
+++ _builder/buildpython.bat
@@ -1,3 +1,15 @@
 @echo off
 
 echo =================== BUILD PYTHON CODE ====================
+
+REM MAKE A PACKAGES IN BUILD/BIN
+IF NOT EXiST bin\python\sorting mkdir bin\python\sorting
+copy /y NUL bin\python\sorting\__init__.py
+copy ..\sorting\python\*.py bin\python\sorting
+
+REM MAKE A PACKAGE INSIDE TESTS
+IF NOT EXiST ..\_tests\sorting\python\bin mkdir ..\_tests\sorting\python\bin
+copy /y NUL ..\_tests\sorting\python\bin\__init__.py
+copy ..\sorting\python\*.py ..\_tests\sorting\python\bin
+
+REM RUN UNIT TESTS
