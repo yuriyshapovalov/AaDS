01e3596888d77ba74d8ed88802a248e6a8a5e757
Yuriy Shapovalov <shapovalov.yuri@gmail.com>
2013-09-26 23:55:59 +0300

    Updated CS build scritp

--- _builder/buildcs.bat
+++ _builder/buildcs.bat
@@ -7,6 +7,8 @@ echo ===================== BUILD C# TESTS =====================
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild csbuild.xml /t:BuildTests
 
 echo ======================== RUN TESTS =======================
-nunit-console tests/csharp/SortingTest.dll
+nunit-console tests/csharp/SortingTest.dll /xml:tests/csharp/SortingTestResult.xml /nologo
+nunit-console tests/csharp/ListsTest.dll /xml:tests/csharp/ListTestResult.xml /nologo
+nunit-console tests/csharp/TreesTest.dll /xml:tests/csharp/TreesTestResult.xml /nologo
 
 
