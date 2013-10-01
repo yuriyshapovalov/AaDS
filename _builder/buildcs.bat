fd55c4cad0b1d76a6a4bc42a07a8ae263b16bf50
Yuriy Shapovalov <shapovalov.yuri@gmail.com>
2013-10-01 10:56:58 +0300

    Update C# build script to reduce dump size

--- _builder/buildcs.bat
+++ _builder/buildcs.bat
@@ -1,10 +1,10 @@
 @echo off
 
 echo ===================== BUILD C# CODE ======================
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild csbuild.xml /t:Build
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild csbuild.xml /t:Build /v:m /nologo
 
 echo ===================== BUILD C# TESTS =====================
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild csbuild.xml /t:BuildTests
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild csbuild.xml /t:BuildTests /v:m /nologo
 
 echo ======================== RUN TESTS =======================
 nunit-console tests/csharp/SortingTest.dll /xml:tests/csharp/SortingTestResult.xml /nologo
