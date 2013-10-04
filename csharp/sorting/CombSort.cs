03d5a8ac54df6062bb8afff5bd70c25a75bdab48
Yuriy Shapovalov <shapovalov.yuri@gmail.com>
2013-10-04 14:54:06 +0300

    Comb sort implemented on C#

--- csharp/sorting/CombSort.cs
+++ csharp/sorting/CombSort.cs
@@ -4,13 +4,36 @@ namespace AaDS.Sorting
 {
     /// <summary>
     /// Comb sort algorithm
-    ///  Θ(n) ~ ____
+    ///  O(n^2)
+    ///  Θ(n^2 / 2^p) where p is a number of increments
     /// </summary>
     public class CombSort<T> : ISort<T> where T : IComparable, new()
     {
         public T[] Sort(T[] array)
         {
-            // not implemented
+            int gap = array.Length;
+            bool swapped = true;
+
+            while (gap > 1 || swapped)
+            {
+                if (gap > 1)
+                    gap = (int) gap / 2;
+
+                int i = 0;
+                swapped = false;
+
+                while (i + gap < array.Length)
+                {
+                    if (array[i].CompareTo(array[i + gap]) > 0)
+                    {
+                        T tmp = array[i];
+                        array[i] = array[i + gap];
+                        array[i + gap] = tmp;
+                        swapped = true;
+                    }
+                    i++;
+                }
+            }
             return array;
         }
     }
