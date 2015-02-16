337c76aeb7c173cfd37b38c7103032ce1469f30f
Yuriy Shapovalov <shapovalov.yuri@gmail.com>
2015-02-16 00:32:51 +0000

    Autogenerator scripts update

---
+++ _builder/autogenerator/creator.py
@@ -0,0 +1,28 @@
+# Autocreator sctipt. Creates stub files
+import sys
+
+class creator:
+	languages = [
+		{'name': 'csharp'},
+		{'name': 'cpp'},
+		{'name': 'java'},
+		{'name': 'python'},
+		{'name': 'javascript'},
+		{'name': 'r'},
+		{'name': 'erlang'},
+		{'name': 'haskell'},
+		{'name': 'agda'},
+		{'name': 'clisp'},
+		{'name': 'fsharp'},
+	]
+	
+	def create():
+		pass
+
+		#if not os.path.exists(directory):
+    	#	os.makedirs(directory)
+
+if __name__ == '__main__':
+	# raise Exception("Run protection: Comment the line to run autocreator script")
+	#
+	create()
