cf601b31f47d65b750057b103b649a4bf99d43c8
Yuriy Shapovalov <shapovalov.yuri@gmail.com>
2015-03-29 23:29:24 +0100

    Fixed Csharp build script

--- env/build/buildcs.py
+++ env/build/buildcs.py
@@ -6,6 +6,7 @@ build_test_start_message = "===================== BUILD C# TESTS ===============
 
 def build_code():
     print(build_code_start_message)
+    call(['xbuild', 'csbuild.xml'])
     #call(['C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild', '/t:Build', '/v:m', '/nologo'])
 
 def build_test():
