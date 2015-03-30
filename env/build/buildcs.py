3c60e60f57700810f857c23b5368819b0f3dfdd5
Yuriy Shapovalov <shapovalov.yuri@gmail.com>
2015-03-30 22:49:15 +0100

    Updated .net build system

--- env/build/buildcs.py
+++ env/build/buildcs.py
@@ -3,6 +3,7 @@ from subprocess import call
 
 build_code_start_message = "===================== BUILD C# CODE ======================"
 build_test_start_message = "===================== BUILD C# TESTS ====================="
+run_tests_start_message  = "===================== RUN UNIT TESTS ====================="
 
 def build_code():
     print(build_code_start_message)
@@ -14,13 +15,14 @@ def build_test():
     call(['xbuild', 'csbuild.xml', '/target:BuildTests', '/verbosity:normal','/nologo'])
     #call(['C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild csbuild.xml', '/t:BuildTests', '/v:m', '/nologo'])
 
+def run_tests():
+    print(run_tests_start_message)
+    call(['nunit-console', 'tests/csharp/SortingTest.dll', '/xml:tests/csharp/SortingTestResult.xml', '/nologo'])
+
 if __name__ == '__main__':
     build_code()
     build_test()
+    run_tests()
 
-# echo ======================== RUN TESTS =======================
-# nunit-console tests/csharp/SortingTest.dll /xml:tests/csharp/SortingTestResult.xml /nologo
-# nunit-console tests/csharp/ListsTest.dll /xml:tests/csharp/ListTestResult.xml /nologo
-# nunit-console tests/csharp/TreesTest.dll /xml:tests/csharp/TreesTestResult.xml /nologo
 
 
